c2f621fefaeadcbd2a3ecfb20bc7978170f5f5d6
Martim Serra <103179241+marl1m@users.noreply.github.com>
2025-08-12 17:01:16 +0100

    bug fixing

--- life_expectancy/constants.py
+++ life_expectancy/constants.py
@@ -1,9 +1,7 @@
 """Constants for life expectancy project."""
 
-from pathlib import Path
-
 # Base data directory
-BASE_DIR = Path("/Users/marlim/dd_lp_foundations_assignments/assignments/life_expectancy/data")
+BASE_DIR = "/Users/marlim/dd_lp_foundations_assignments/assignments/life_expectancy/data"
 #"./life_expectancy/data/eu_life_expectancy_raw.tsv"
 
 # Input file path
